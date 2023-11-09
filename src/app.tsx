972b18e69ab6cc35dd71fc1870522f607f770282
Tom Hedges <tom.hedges@blackbaud.co.uk>
2023-11-09 00:26:04 +0000

    try again...

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom";
 const App = () => {
   return (
     <>
-      <h1>Hello, World! ....</h1>
+      <h1>Hello, World!</h1>
       <h2>twitter-rank</h2>
+      <p>Test  1</p>
     </>
   );
 };
